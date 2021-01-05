c3736aa4d257f63dfff6bc13c43e15588a7d5a6a
ravi mishra <ravi@zethic.com>
2021-01-06 01:17:39 +0530

    Added functions to upload functions

--- app/src/main/java/com/ravi/campusassist/MainActivity.java
+++ app/src/main/java/com/ravi/campusassist/MainActivity.java
@@ -1,12 +1,12 @@
 package com.ravi.campusassist;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,11 +17,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -32,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseStorage storage;
     FirebaseDatabase database;
-    Uri pdfUri;
+     Uri pdfUri;
+
+    ProgressDialog progressDialog;
 
 
     @Override
@@ -42,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         storage = FirebaseStorage.getInstance();
         database = FirebaseDatabase.getInstance();
         selectFile = findViewById(R.id.selectFile);
+        uploadFileBtn = findViewById(R.id.uploadFile);
 
         selectFile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,23 +73,92 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void uploadFile(Uri pdfUri) {
+        progressDialog= new ProgressDialog(MainActivity.this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+         progressDialog.setTitle("Uploading file");
+         progressDialog.setProgress(0);
+         progressDialog.show();
         String filename = System.currentTimeMillis()+"";
         StorageReference storageReference = storage.getReference();
+        UploadTask uploadTask =   storageReference.child("PDFS").child(filename).putFile(pdfUri);
         storageReference.child("PDFS").child(filename).putFile(pdfUri)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> task =  uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                            @Override
+                            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                if (!task.isSuccessful()) {
+                                    throw task.getException();
+                                }
 
+                                // Continue with the task to get the download URL
+                                return storageReference.getDownloadUrl();
+//storage.getReference().getDownloadUrl();
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                if (task.isSuccessful()) {
+                                    Uri pdfUri = task.getResult();
+                                    DatabaseReference reference = database.getReference();
+                                    reference.child(filename).setValue(pdfUri).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(MainActivity.this, "file successfully uploaded", Toast.LENGTH_SHORT).show();
+                                            }else{
+                                                Toast.makeText(MainActivity.this, "file not uploaded", Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        }
+                                    });
+
+                                } else {
+                                    Log.v("stan","file not uploaded");
+                                    Toast.makeText(MainActivity.this, "file not uploaded", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
                     }
-                }).addOnFailureListener(new OnFailureListener() {
+                }).addOnFailureListener(e ->
+                Toast.makeText(MainActivity.this, "file not uploaded", Toast.LENGTH_SHORT).show()).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
             @Override
-            public void onFailure(@NonNull Exception e) {
-                
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                if (snapshot.getTotalByteCount() != 0) {
+                    if (snapshot.getBytesTransferred() == snapshot.getTotalByteCount() ){
+                        progressDialog.dismiss();
+
+                    }else{
+                        int currentProgress = (int) (100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                        progressDialog.setProgress(currentProgress);
+                    }
+
+                }
             }
         });
 
     }
+/*
+Task<Uri> url= storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                DatabaseReference reference = database.getReference();
+                                reference.child(filename).setValue(uri).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            Toast.makeText(MainActivity.this, "file successfully uploaded", Toast.LENGTH_SHORT).show();
+                                        }else{
+                                            Toast.makeText(MainActivity.this, "file not uploaded", Toast.LENGTH_SHORT).show();
 
+                                        }
+                                    }
+                                });
+                            }
+                        });
+ */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -120,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data!=null) {
             pdfUri = data.getData();
+            Toast.makeText(this, "A file is successfully selected"+data.getData().getLastPathSegment() , Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(this, "Please a select a file", Toast.LENGTH_SHORT).show();
         }
